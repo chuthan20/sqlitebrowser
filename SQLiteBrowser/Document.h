d0f8d7e0acc517da42b7925944878d3396f8e7d5
Archuthan Vijayaratnam <archuthanvijayaratnam@archu.gateway.2wire.net>
2012-10-27 23:20:27 -0400

    Initial Commit

---
+++ SQLiteBrowser/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  SQLiteBrowser
+//
+//  Created by Archuthan Vijayaratnam on 2012-10-27.
+//  Copyright (c) 2012 Archuthan Vijayaratnam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ SQLiteBrowser/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SQLiteBrowser
+//
+//  Created by Archuthan Vijayaratnam on 2012-10-27.
+//  Copyright (c) 2012 Archuthan Vijayaratnam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
