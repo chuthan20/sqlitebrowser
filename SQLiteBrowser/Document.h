deb2f7707d31799141c35803128a68c64c69f484
Archuthan Vijayaratnam <chuthan20@gmail.com>
2014-02-16 00:58:57 -0500

    Fixed few UI issues and added some features
    Fixed:
    - closing will not ask to save, and autosave is disabled
    - removed Page stepper and unused code
    - removed words completion feature
    - added pragma statments
    - moved opening database into function
    - using alt+enter, multiple statements can be written — having the text insertion point at a query and pressing enter will execute that query
    - result tableview header identifier used to be index of column title, now, its the column title itself (this will help when editing field from tableview is allowed to save changes in the database).
    - private ivars starts with underscore

--- SQLiteBrowser/Document.h
+++ SQLiteBrowser/Document.h
@@ -9,31 +9,11 @@
 #import <Cocoa/Cocoa.h>
 
 @interface Document : NSDocument <NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate, NSOutlineViewDataSource, NSOutlineViewDelegate, NSTextViewDelegate>
-{
-    NSMutableArray *recentSearches;
-    NSMutableArray *arrayOfData;
-    
-    NSString *databaseFileName;
-    
-    NSMutableArray *leftOutline;
-    
-    NSString *lastTableToBeClicked;
-    int rowIdOfLastItemClicked;
-    
-    
-    NSArray *sideTableTitles;
-    
-    
-    
-	BOOL					completePosting;
-    BOOL					commandHandling;
-}
+
 @property (weak) IBOutlet NSView *docView;
 
 @property (strong) IBOutlet NSTextView *stmtQueryField;
 
-@property (weak) IBOutlet NSTextField *pagingTextField;
-@property (weak) IBOutlet NSStepper *pagingStepper;
 @property (weak) IBOutlet NSOutlineView *leftOutlineView;
 @property (weak) IBOutlet NSTableView *mainTable;
 
--- SQLiteBrowser/Document.m
+++ SQLiteBrowser/Document.m
@@ -9,68 +9,71 @@
 #import "Document.h"
 #include <sqlite3.h>
 
-static int kNumOffset = 100;
+static int kNumOffset = 5000;
+
+@interface Document ()
+{
+    NSMutableArray*             _arrayOfData;
+    NSMutableArray*             _leftOutline;
+
+    NSArray*                    _sideTableTitles;
+
+    NSString*                   _databaseFileName;
+    NSString*                   _lastTableToBeClicked;
+
+    int                         _rowIdOfLastItemClicked;
+
+	BOOL                        _completePosting;
+    BOOL                        _commandHandling;
+}
+@end
+
 
 @implementation Document
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Add your subclass-specific initialization here.
-    }
-    return self;
-}
-
 - (NSString *)windowNibName
 {
-    return @"Document";
+    return NSStringFromClass([self class]);
 }
 
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
 {
     [super windowControllerDidLoadNib:aController];
     
-    arrayOfData = [NSMutableArray array];
-    leftOutline = [NSMutableArray array];
+    _arrayOfData = [NSMutableArray array];
+    _leftOutline = [NSMutableArray array];
     
-    self.pagingStepper.maxValue = 0.f;
-    self.pagingStepper.minValue = 0.f;
-    
-    if (databaseFileName)
+    if (_databaseFileName != NULL)
     {
         [self loadBtnClicked:nil];
     }
     
-    sideTableTitles = @[@"Table", @"View", @"Index"] ;
-    recentSearches = [NSMutableArray arrayWithObjects:@"sqlite_master ", @"select ",@"from ",@"where ",@"union ",@"update ",@"delete ",@"drop ",@"table ",@"explain ",@"set ",@"count ",@"order by ",@"limit ",@"offset ",@"rowid ", nil];
+    _sideTableTitles = @[@"Table", @"View", @"Index"] ;
     _mainTable.rowHeight = 22;
 }
 
 + (BOOL)autosavesInPlace
 {
-    return YES;
+    return NO;
 }
 
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
+- (void)canCloseDocumentWithDelegate:(id)delegate shouldCloseSelector:(SEL)shouldCloseSelector contextInfo:(void *)contextInfo
 {
-    NSLog(@"%s (%@)(%@)" , __PRETTY_FUNCTION__, typeName, *outError);
-    return nil;
-//    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-//    @throw exception;
-//    return nil;
+    [self updateChangeCount:NSChangeCleared];
+    [self close];
 }
 
 - (BOOL)readFromURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError *__autoreleasing *)outError
 {
-    databaseFileName = url.path;
+    _databaseFileName = url.path;
     return YES;
 }
 
-- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
-    
+#pragma mark - TableView Datasource & Delegates
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
     if (tableView == self.mainTable)
-        return arrayOfData.count;
+        return _arrayOfData.count;
     return 0;
 }
 
@@ -84,7 +87,8 @@ static int kNumOffset = 100;
     NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:[NSFont systemFontOfSize:13], NSFontAttributeName, nil];
     for (int i=0; i<rows; i++)
     {
-        NSString *strValue = [[arrayOfData objectAtIndex:i] objectForKey:c.identifier];
+
+        NSString *strValue = [[_arrayOfData objectAtIndex:i] objectForKey:c.identifier];
         NSSize labelSize = [strValue sizeWithAttributes:attributes];
         maxWidth = MAX(labelSize.width + 10, maxWidth);
     }
@@ -95,51 +99,101 @@ static int kNumOffset = 100;
 {
     if ([tableView isEqualTo:self.mainTable])
     {
-        NSString *strValue = [[arrayOfData objectAtIndex:row] objectForKey:tableColumn.identifier];
+        NSString *strValue = [[_arrayOfData objectAtIndex:row] objectForKey:tableColumn.identifier];
         return strValue;
     }
     return nil;
 }
 
 
+- (void) tableView:(NSTableView *)tableView setObjectValue:(id)object forTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+
+     NSLog(@"%@", [_arrayOfData objectAtIndex:row]);
+     NSLog(@"%ld", (long)row);
+     NSLog(@"%@", object);
+     NSLog(@"%@", tableColumn);
+}
+
+#pragma mark - Toolbar button events
 - (IBAction)loadBtnClicked:(id)sender
 {
     NSLog(@"%s" , __PRETTY_FUNCTION__);
-    lastTableToBeClicked = @"sqlite_master";
-    [self loadAndDisplayTable:lastTableToBeClicked offset:0 limit:kNumOffset];
+    _lastTableToBeClicked = @"sqlite_master";
+    [self loadAndDisplayTable:_lastTableToBeClicked offset:0 limit:kNumOffset];
     [self loadAndDisplayLeftTable];
+    
 }
 
-- (IBAction)executeBtnClicked:(id)sender {
-    NSString *original_stmt = [[self.stmtQueryField textStorage] string];
-    NSString *stmt = [original_stmt substringWithRange:[self.stmtQueryField selectedRange]];
-    if (stmt == nil)
-        stmt = original_stmt;
-    [recentSearches addObject:stmt];
-    [self loadAndDisplayTableWithQuery:stmt];
+- (IBAction)executeBtnClicked:(id)sender
+{
+    NSRange range = [self.stmtQueryField selectedRange];
+    if (range.length > 0)
+    {
+        [self loadAndDisplayTableWithQuery:[[self.stmtQueryField string] substringWithRange:range]];
+    }
+    else
+    {
+        NSMutableString *query = [NSMutableString string];
+
+        //assume str is    beg....  x ....end
+        NSString *str = [self.stmtQueryField string];
+
+        //copy from from end to x
+        for (int i = (int)range.location; i<str.length; i++)
+        {
+             NSLog(@"%c", [str characterAtIndex:i]);
+            if ([str characterAtIndex:i] == '\n')
+                break;
+            else
+                [query appendFormat:@"%c", [str characterAtIndex:i]];
+        }
+
+        // copy string from x to beginning
+        for (int i = (int)range.location-1; i >= 0; i--)
+        {
+            if ([str characterAtIndex:i] == '\n')
+                break;
+
+            [query insertString:[NSString stringWithFormat:@"%c", [str characterAtIndex:i]] atIndex:0];
+
+            if (i==0)
+                break;
+        }
+        [self loadAndDisplayTableWithQuery:query];
+    }
+
+//    NSString *original_stmt = [[self.stmtQueryField textStorage] string];
+//    NSString *stmt = [original_stmt substringWithRange:[self.stmtQueryField selectedRange]];
+//
+//    if (range.length == 0)
+//        stmt = original_stmt;
+//
+//    [self loadAndDisplayTableWithQuery:stmt];
 }
 
-- (IBAction)pagingStepperClicked:(NSStepper *)sender {
-    [self.pagingTextField setIntValue:sender.intValue];
-    [self loadAndDisplayTable:lastTableToBeClicked offset:0 limit:kNumOffset];
-}
 
+- (IBAction)toolbarItemClicked:(NSToolbarItem *)sender
+{
+    if ([@"execute" isEqualToString:[sender.label lowercaseString]])
+    {
+        [self executeBtnClicked:nil];
+    }
+    else if ([@"reset" isEqualToString:[sender.label lowercaseString]])
+    {
+        [self loadBtnClicked:nil];
+    }
+}
 
 - (void) loadAndDisplayLeftTable
 {
-    sqlite3_stmt    *statement;
-    sqlite3 *fdb;
-    NSString *databasePath = databaseFileName;
-    
-//    [[NSFileManager defaultManager] fileExistsAtPath:databasePath] ? NSLog(@"File Exists") : NSLog(@"File DOES NOT Exists");
-    const char *dbpath = [databasePath UTF8String];
-    
-    
-    int ret = sqlite3_open(dbpath, &fdb);
-    if (ret == SQLITE_OK)
+    sqlite3 *fdb = [self openDatabase];
+    if (fdb != NULL)
     {
         NSString *query = @"SELECT tbl_name, type FROM sqlite_master";
         NSLog(@"%@", query);
+
+        sqlite3_stmt    *statement = NULL;
         if (sqlite3_prepare_v2(fdb, [query UTF8String], -1, &statement, NULL) == SQLITE_OK)
         {
             
@@ -168,33 +222,26 @@ static int kNumOffset = 100;
                     [indices addObject:name];
                 }
             }
-            [leftOutline removeAllObjects];
-            [leftOutline addObject:tbls];
-            [leftOutline addObject:views];
-            [leftOutline addObject:indices];
+            [_leftOutline removeAllObjects];
+            [_leftOutline addObject:tbls];
+            [_leftOutline addObject:views];
+            [_leftOutline addObject:indices];
             sqlite3_finalize(statement);
         }
     }
     sqlite3_close(fdb);
-    
     [_leftOutlineView reloadData];
 }
 
 - (int) getCount:(NSString *)queryString
 {
-    sqlite3_stmt    *statement;
-    sqlite3 *fdb;
-    NSString *databasePath = databaseFileName;
-    
-//    [[NSFileManager defaultManager] fileExistsAtPath:databasePath] ? NSLog(@"File Exists") : NSLog(@"File DOES NOT Exists");
-    
-    const char *dbpath = [databasePath UTF8String];
+    sqlite3 *fdb = [self openDatabase];
     int numOfRows = -1;
-    int ret = sqlite3_open(dbpath, &fdb);
-    if (ret == SQLITE_OK)
+
+    if (fdb != NULL)
     {
+        sqlite3_stmt    *statement = NULL;
         NSString *qry = [NSString stringWithFormat:@"select count(*) from %@", queryString];
-        NSLog(@"%@", qry);
         if (sqlite3_prepare_v2(fdb, [qry UTF8String], -1, &statement, NULL) == SQLITE_OK)
         {
             while (sqlite3_step(statement) == SQLITE_ROW)
@@ -214,160 +261,113 @@ static int kNumOffset = 100;
 {
     if (!tableName)
         return;
-    
-    for (int x= (int)self.mainTable.tableColumns.count-1; x>= 0; x--)
-    {
-        NSTableColumn *obj = [[self.mainTable tableColumns] objectAtIndex:x];
-        [self.mainTable removeTableColumn:obj];
-    }
-    
-    [arrayOfData removeAllObjects];
-    [self.mainTable reloadData];
-    sqlite3_stmt    *statement;
-    sqlite3 *fdb;
-    
-    NSString *databasePath = databaseFileName;
-    
-//    [[NSFileManager defaultManager] fileExistsAtPath:databasePath] ? NSLog(@"File Exists") : NSLog(@"File DOES NOT Exists");
-    
-    
-    const char *dbpath = [databasePath UTF8String];
-    
-    
-    int ret = sqlite3_open(dbpath, &fdb);
-    if (ret == SQLITE_OK)
-    {
-        NSString *query = [NSString stringWithFormat:@"SELECT * FROM %@ ORDER BY rowid LIMIT %d OFFSET %d", tableName, limit, offset];
-        NSLog(@"%@", query);
-        
-        if (sqlite3_prepare_v2(fdb, [query UTF8String], -1, &statement, NULL) == SQLITE_OK)
-        {
-            int count = sqlite3_column_count(statement);
-            for(int i=0; i<count; i++)
-            {
-                NSTableColumn *col1 = [[NSTableColumn alloc] initWithIdentifier:[NSString stringWithFormat:@"%d", i]];
-                const char *name = sqlite3_column_name(statement, i);
-                [[col1 headerCell] setStringValue: [NSString stringWithFormat:@"%s", name]];
-                [[col1 headerCell] setRepresentedObject:[NSString stringWithUTF8String:name]];
-                [self.mainTable addTableColumn:col1];
-            }
-            
-            while (sqlite3_step(statement) == SQLITE_ROW)
-            {
-                NSMutableDictionary *data = [NSMutableDictionary dictionary];
-                for(int i=0; i<count; i++)
-                {
-                    [data setObject:[self getValue:statement index:i] forKey:[NSString stringWithFormat:@"%d", i]];
-                }
-                
-                [arrayOfData addObject:data];
-            }
-            sqlite3_finalize(statement);
-        }
-    }
-    sqlite3_close(fdb);
-    
-    
-    [self.mainTable reloadData];
-    
-    [self.pagingTextField setIntValue:self.pagingStepper.intValue];
+
+    NSString *query = [NSString stringWithFormat:@"SELECT * FROM %@ ORDER BY rowid LIMIT %d OFFSET %d", tableName, limit, offset];
+    [self loadAndDisplayTableWithQuery:query];
 }
 
+- (void) setResultsTableHeadersWithTitle:(sqlite3_stmt *)statement
+{
+    for(int i=0; i<sqlite3_column_count(statement); i++)
+    {
+        const char *name = sqlite3_column_name(statement, i);
+        NSString *identifier = [[NSString alloc] initWithFormat:@"%s",name];
+
+        NSTableColumn *col1 = [[NSTableColumn alloc] initWithIdentifier:identifier];
+        [[col1 headerCell] setStringValue: identifier];
+        [[col1 headerCell] setRepresentedObject:identifier];
+
+        [self.mainTable addTableColumn:col1];
+    }
+}
+
+- (void) setResultsTableBody:(sqlite3_stmt *)statement
+{
+    while (sqlite3_step(statement) == SQLITE_ROW)
+    {
+        NSMutableDictionary *data = [NSMutableDictionary dictionary];
+        for(int i=0; i<sqlite3_column_count(statement); i++)
+        {
+            const char *name = sqlite3_column_name(statement, i);
+            NSString *identifier = [[NSString alloc] initWithFormat:@"%s",name];
+            [data setObject:[self getValue:statement index:i] forKey:identifier];
+        }
+        [_arrayOfData addObject:data];
+    }
+}
+
+- (sqlite3 *) openDatabase
+{
+    sqlite3 *fdb;
+    const char *dbpath = [_databaseFileName UTF8String];
+    int ret = sqlite3_open(dbpath, &fdb);
+    if (ret != SQLITE_OK)
+    {
+        NSLog(@"Status = %d, error = %s", ret, sqlite3_errmsg(fdb));
+        sqlite3_close(fdb);
+        fdb = NULL;
+    }
+    return fdb;
+}
 
 - (void) loadAndDisplayTableWithQuery:(NSString *)query
 {
-    if (!query)
-        return;
-    
+    NSLog(@"%@", query);
     for (int x= (int)self.mainTable.tableColumns.count-1; x>= 0; x--)
     {
         NSTableColumn *obj = [[self.mainTable tableColumns] objectAtIndex:x];
         [self.mainTable removeTableColumn:obj];
     }
     
-    [arrayOfData removeAllObjects];
+    [_arrayOfData removeAllObjects];
     [self.mainTable reloadData];
     
-    sqlite3_stmt    *statement;
-    sqlite3 *fdb;
-    NSString *databasePath = databaseFileName;
-    
-//    [[NSFileManager defaultManager] fileExistsAtPath:databasePath] ? NSLog(@"File Exists") : NSLog(@"File DOES NOT Exists");
-    
-    const char *dbpath = [databasePath UTF8String];
-    int ret = sqlite3_open(dbpath, &fdb);
-    if (ret == SQLITE_OK)
+    sqlite3 *fdb = [self openDatabase];
+    if (fdb != NULL)
     {
-        //        NSString *query = [NSString stringWithFormat:query];
-        NSLog(@"%@", query);
-        if (sqlite3_prepare_v2(fdb, [query UTF8String], -1, &statement, NULL) == SQLITE_OK)
+        sqlite3_stmt    *statement = NULL;
+        int status = sqlite3_prepare_v2(fdb, [query UTF8String], -1, &statement, NULL);
+        if (status == SQLITE_OK)
         {
-            for(int i=0; i<sqlite3_column_count(statement); i++)
-            {
-                NSTableColumn *col1 = [[NSTableColumn alloc] initWithIdentifier:[NSString stringWithFormat:@"%d", i]];
-                sqlite3_column_value(statement, i);
-                const char *name = sqlite3_column_name(statement, i);
-                const char *type = sqlite3_column_decltype(statement, i);
-                
-                if (name == NULL)
-                    name = "-";
-                if (type == NULL)
-                    type = "-";
-                
-                [[col1 headerCell] setStringValue: [NSString stringWithFormat:@"%@%@",[NSString stringWithUTF8String:name], @""]];//[[NSString stringWithUTF8String:type] uppercaseString]]];
-                [[col1 headerCell] setRepresentedObject:[NSString stringWithUTF8String:name]];
-                [self.mainTable addTableColumn:col1];
-            }
-            
-            while (sqlite3_step(statement) == SQLITE_ROW)
-            {
-                NSMutableDictionary *data = [NSMutableDictionary dictionary];
-                for(int i=0; i<sqlite3_column_count(statement); i++)
-                {
-                    [data setObject:[self getValue:statement index:i] forKey:[NSString stringWithFormat:@"%d", i]];
-                }
-                [arrayOfData addObject:data];
-            }
+            [self setResultsTableHeadersWithTitle:statement];
+            [self setResultsTableBody:statement];
             sqlite3_finalize(statement);
+        }
+        else
+        {
+            NSLog(@"Status = %d; error = %s", status, sqlite3_errmsg(fdb));
         }
     }
     sqlite3_close(fdb);
     [self.mainTable reloadData];
-    
-    //    [self.pagingTextField setIntValue:self.pagingStepper.intValue];
 }
 
 - (id) getValue:(sqlite3_stmt *)stmt index:(int)ind
 {
+    //TODO: should check for type, i.e., blob? then give options for preview... image, audio.. etc
     const char *cname =  (const char *) sqlite3_column_text(stmt, ind);
-    if (cname != NULL)
-    {
-        NSString *str = [[NSString alloc] initWithUTF8String:cname];
-        if (str)
-            return str;
-        else
-        {
-            return [NSString stringWithFormat:@"%s", (const char *) sqlite3_column_text(stmt, ind)];
-        }
-    }
-    return @"-";
+    return [[NSString alloc] initWithBytes:cname length:strlen(cname) encoding:NSUTF8StringEncoding];
 }
 
-- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item {
-    if(item == nil)  return leftOutline.count;
-    return [[leftOutline objectAtIndex:[sideTableTitles indexOfObject:item]] count];
+#pragma mark - Left outline view Datasource & delegates
+- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item
+{
+    if(item == nil)  return _leftOutline.count;
+    return [[_leftOutline objectAtIndex:[_sideTableTitles indexOfObject:item]] count];
 }
 
-- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item {
-    return [sideTableTitles containsObject:item];
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item
+{
+    return [_sideTableTitles containsObject:item];
 }
 
-- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item {
+- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item
+{
     if (item == nil)
-        return [sideTableTitles objectAtIndex:index];
+        return [_sideTableTitles objectAtIndex:index];
     else
     {
-        return [[leftOutline objectAtIndex:[sideTableTitles indexOfObject:item]] objectAtIndex:index];
+        return [[_leftOutline objectAtIndex:[_sideTableTitles indexOfObject:item]] objectAtIndex:index];
     }
 }
 
@@ -378,37 +378,24 @@ static int kNumOffset = 100;
 
 - (void)outlineViewSelectionDidChange:(NSNotification *)notification
 {
-    
     NSOutlineView *ov = notification.object;
     NSString *item = [ov itemAtRow:ov.selectedRow];
     NSString *parent = [ov parentForItem:item];
     
-    if (parent != nil && [parent isEqualToString:[sideTableTitles objectAtIndex:0]])
+    if (parent != nil && [parent isEqualToString:[_sideTableTitles objectAtIndex:0]])
     {
-        lastTableToBeClicked = item;
+        _lastTableToBeClicked = item;
         [self loadAndDisplayTable: item offset:0 limit:kNumOffset];
     }
     else if ([@"table" isEqualToString:[item lowercaseString]] || [@"index" isEqualToString:[item lowercaseString]] || [@"view" isEqualToString:[item lowercaseString]])
     {
-        [self loadAndDisplayTableWithQuery:[NSString stringWithFormat:@"select * from sqlite_master type = '%@'", item]];
+        [self loadAndDisplayTableWithQuery:[NSString stringWithFormat:@"select * from sqlite_master where type like '%@'", item]];
     }
 }
 
-- (IBAction)toolbarItemClicked:(NSToolbarItem *)sender
-{
-    if ([@"execute" isEqualToString:[sender.label lowercaseString]])
-    {
-        [self executeBtnClicked:nil];
-    }
-    else if ([@"reset" isEqualToString:[sender.label lowercaseString]])
-    {
-        [self loadBtnClicked:nil];
-    }
-}
-
+#pragma mark - Query textview
 - (BOOL)textView:(NSTextView *)aTextView doCommandBySelector:(SEL)aSelector
 {
-    NSLog(@"%@" , NSStringFromSelector(_cmd));
     if (aSelector == @selector(insertNewline:))
     {
         [self executeBtnClicked:nil];
@@ -420,12 +407,6 @@ static int kNumOffset = 100;
         return YES;
     }
     return NO;
-}
-
-- (NSArray *)textView:(NSTextView *)textView completions:(NSArray *)words forPartialWordRange:(NSRange)charRange indexOfSelectedItem:(NSInteger *)index
-{
-    NSLog(@"%@" , words);
-    return nil;
 }
 
 @end
